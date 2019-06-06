e8d7ab6295ea9ab46d633fb809b2f440b42ae347
M Banks <montensia1@outlook.com>
2019-06-06 19:51:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,11 +8,7 @@ require_relative './course.rb'
 class Scraper
   attr_reader :course
     def get_page
-      doc = Nokogiri::HTML(open("https://sweetpotatosoul.com/recipes"))
-      
-      doc.css("").each do |recipe|
-        
-      end
+      doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
       # doc.css(".post").each do |post|
       #   course = Course.new
       #   course.title = post.css('h2').text 
